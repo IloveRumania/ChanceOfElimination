4d72dd6f766373bebcc15b5d45095fdbecd0b18f
Matthew Schafer <lucidsigma17@gmail.com>
2021-12-04 14:37:37 +1000

    Create athlete struct

---
+++ src/athlete.rs
@@ -0,0 +1,25 @@
+use std::cmp::Ordering;
+
+#[derive(Eq)]
+pub struct Athlete<'a> {
+    pub score: u32,
+    pub name: &'a str,
+}
+
+impl PartialEq for Athlete<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.score == other.score
+    }
+}
+
+impl PartialOrd for Athlete<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.score.cmp(&other.score))
+    }
+}
+
+impl Ord for Athlete<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.score.cmp(&other.score)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use std::cmp::Ordering;
+mod athlete;
 
-#[derive(Eq)]
-struct Athlete<'a> {
-    score: u32,
-    name: &'a str,
-}
-
-impl PartialEq for Athlete<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.score == other.score
-    }
-}
-
-impl PartialOrd for Athlete<'_> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.score.cmp(&other.score))
-    }
-}
-
-impl Ord for Athlete<'_> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.score.cmp(&other.score)
-    }
-}
+use athlete::Athlete;
 
 fn main() {
-    println!("Hello, world!");
+    
 }
